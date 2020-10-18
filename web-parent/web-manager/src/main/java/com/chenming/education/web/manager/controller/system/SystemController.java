58f0015438137a0e2004e65c06c8dc8448c342fb
chenming <mover93@126.com>
2020-10-18 18:10:00 +0800

    fix

--- web-parent/web-manager/src/main/java/com/chenming/education/web/manager/controller/system/SystemController.java
+++ web-parent/web-manager/src/main/java/com/chenming/education/web/manager/controller/system/SystemController.java
@@ -1,7 +1,7 @@
-package com.chenming.education.system.controller;
+package com.chenming.education.web.manager.controller.system;
 
-import com.chenming.education.system.business.SystemBusiness;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.chenming.education.user.service.UserService;
+import org.apache.dubbo.config.annotation.DubboReference;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,18 +10,18 @@ import org.springframework.web.bind.annotation.RestController;
 /**
  * description: SystemController <br>
  * author: chenming <br>
- * date: 2020/10/18 17:02 <br>
+ * date: 2020/10/18 17:39 <br>
  * version: 1.0 <br>
  */
 @RestController
-@RequestMapping(value = "/system")
+@RequestMapping(value = "/api/system")
 public class SystemController {
 
-    @Autowired
-    private SystemBusiness systemBusiness;
+    @DubboReference
+    private UserService userService;
 
     @GetMapping(value = "/{msg}")
     public String echo(@PathVariable(value = "msg") String msg) {
-        return systemBusiness.echo(msg);
+        return userService.echo(msg);
     }
 }
